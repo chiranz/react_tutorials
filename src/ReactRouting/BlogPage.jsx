16e3778cb7bdabfc14a6cadebfcba075d211e20f
chiranz <chiranpoudyal@gmail.com>
2021-08-04 15:48:48 +0530

    Context handeling update

--- src/ReactRouting/BlogPage.jsx
+++ src/ReactRouting/BlogPage.jsx
@@ -3,13 +3,15 @@ import { BlogContext } from "./context/BlogContext";
 
 export default function BlogPage() {
   const blogId = 1;
-  const { blogs } = React.useContext(BlogContext);
+  const { blogs, setBlogs } = React.useContext(BlogContext);
+  const [blog, setBlog] = React.useState(null);
+  React.useEffect(() => {
+    setBlog(blogs.find((blog) => blog.id === blogId));
+  });
 
   return (
     <div>
-      {blogs[blogId].heading}
-      {blogs[blogId].author}
-      {blogs[blogId].body}
+      <h1>{blog.heading}</h1>
     </div>
   );
 }
--- src/ReactRouting/context/BlogContext.jsx
+++ src/ReactRouting/context/BlogContext.jsx
@@ -3,23 +3,26 @@ import React from "react";
 export const BlogContext = React.createContext(null);
 
 export const BlogProvider = ({ children }) => {
-  const [blogs, setBlogs] = React.useState({
-    1: {
+  const [blogs, setBlogs] = React.useState([
+    {
+      id: 1,
       heading: "This is heading",
       body: "this is body",
       author: "Someone ",
     },
-    2: {
+    {
+      id: 2,
       heading: "This is heading",
       body: "this is body",
       author: "Someone ",
     },
-    3: {
+    {
+      id: 3,
       heading: "This is heading",
       body: "this is body",
       author: "Someone ",
     },
-  });
+  ]);
 
   return (
     <BlogContext.Provider value={{ blogs, setBlogs }}>
